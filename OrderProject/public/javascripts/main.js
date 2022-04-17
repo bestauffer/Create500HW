440bd887118199bc07a3995549f3c249913d1144
tashibalam <tashib.alam@bellevuecollege.edu>
2022-04-16 22:47:03 -0700

    sort query button added

--- OrderProject/public/javascripts/main.js
+++ OrderProject/public/javascripts/main.js
@@ -1,57 +1,53 @@
-let storeIdValues = [98053, 98007, 98077, 98055, 98011, 98046];
-let cdIdValues = [123456, 123654, 321456, 321654, 654123, 654321, 543216, 354126, 621453, 623451];
+const storeIdValues = [98053, 98007, 98077, 98055, 98011, 98046];
+const cdIdValues = [123456, 123654, 321456, 321654, 654123, 654321, 543216, 354126, 621453, 623451];
+let orderArray = [];
 
 // define a constructor to create movie objects
-let OrderObject = function(){
-    this.StoreID;
-    this.SalesPersonID;
-    this.CdID;
-    this.PricePaid;
-    this.Date;
+class OrderObject {
+    constructor() {
+        this.ID = Math.random().toString(16).slice(5);
+        this.StoreID;
+        this.SalesPersonID;
+        this.CdID;
+        this.PricePaid;
+        this.Date;
+    }
 }
 
 //continuously updates the time
 setInterval(function(){
-    document.getElementById("order-time").value = new Date();
+    document.getElementById("order-time").value = new Date().toISOString();
 }, 100);
 
 function Get500Orders(){
     //After the for loop, this will contain 500 orders
-    let orderArray = new Array(500);
     let currentTime = new Date(document.getElementById("order-time").value);
-    for(let i = 0; i < orderArray.length; i++){
+    for(let i = 0; i < 500; i++){
         //Set the time
         let newOrder = new OrderObject();
         if (i != 0){
             currentTime.setSeconds(currentTime.getSeconds() + Math.floor(Math.random() * 1501  + 300));            
         }
-        newOrder.Date = currentTime;
+        newOrder.Date = currentTime.toISOString();
         newOrder.StoreID = document.getElementById("storeID").value;
         newOrder.SalesPersonID = document.getElementById("salesPersonID").value;
         newOrder.CdID = document.getElementById("cdID").value;
         newOrder.PricePaid = document.getElementById("pricePaid").value;
 
-        //Add the order to the array at index i
-        orderArray[i] = newOrder;
-
         //Add code to send newOrder to node server below here
         //Or try to send the whole array instead further down
-        fetch('/AddOrder', {
+        fetch('/addOrder', {
             method: "POST",
             body: JSON.stringify(newOrder),
             headers: {"Content-type": "application/json; charset=UTF-8"}
-            })
-            .then(response => response.json()) 
-            .then(json => console.log(json)
-            )
-            .catch(err => console.log(err));
+        })
+        .then(response => response.json()) 
+        .then(json => console.log(json), createList())
+        .catch(err => console.log(err));
 
         //makes new random data points & sends them to 
         SetOrderValues(); 
     }
-        //Or add code below here to send orderArray to node server
-
-
 }
 
 //set the remaining values for an order object when CREATE button is clicked
@@ -79,18 +75,17 @@ function SetOrderValues(){
             break;
         case 98011:
             //17-20
-            order.SalesPersonID = Math.floor(Math.random() * 4) + 17;
+            order.SalesPersonID = Math.floor(Math.random() * 4 + 17);
             break;
         case 98046:
             //21-24
-            order.SalesPersonID = Math.floor(Math.random() * 4) + 21;
-            break;
+            order.SalesPersonID = Math.floor(Math.random() * 4 + 21);
     }
     //0-9
     order.CdID = cdIdValues[Math.floor(Math.random() * 10)];
     //5-15
-    order.PricePaid = Math.floor(Math.random() * 11) + 5;
-
+    order.PricePaid = Math.floor(Math.random() * 11 + 5);
+    //Update the Elements with the new values
     document.getElementById("storeID").value = order.StoreID; 
     document.getElementById("salesPersonID").value = order.SalesPersonID;
     document.getElementById("cdID").value = order.CdID;
@@ -112,54 +107,72 @@ function SubmitOne(){
         method: "POST",
         body: JSON.stringify(newOrder),
         headers: {"Content-type": "application/json; charset=UTF-8"}
-        })
-        .then(response => response.json()) 
-        .then(json => console.log(json)
-        )
-        .catch(err => console.log(err));
-        console.log(newOrder);
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json), createList())
+    .catch(err => console.log(err));
+    console.log(newOrder);
+}
 
+function sortPriceSalesPerson(){
+        // update local array from server
+        fetch('/sortPriceSalesPerson')
+        // Handle success
+        .then(response => response.json())  // get the data out of the response object
+        .then( responseData => fillUL(responseData))    //update our array and li's
+        .catch(err => console.log('Request Failed', err)); // Catch errors
 }
 
 //A function that says what the page does when it loads
 document.addEventListener("DOMContentLoaded", function () {
     SetOrderValues();
+    createList();
 
-
-
-// add button events ************************************************************************
-    // document.getElementById("buttonSubmitOne").addEventListener("click", function () {
-    //     let newOrder = new OrderObject();
-    //     newOrder.StoreID = document.getElementById("storeID").value;
-    //     newOrder.SalesPersonID = document.getElementById("salesPersonID").value;
-    //     newOrder.CdID = document.getElementById("cdID").value;
-    //     newOrder.PricePaid = document.getElementById("pricePaid").value;
-    //     newOrder.Date = document.getElementById("order-time").value;
-
-
-    //     // fetch('/AddOrder', {
-    //     //     method: "POST",
-    //     //     body: JSON.stringify(newOrder),
-    //     //     headers: {"Content-type": "application/json; charset=UTF-8"}
-    //     //     })
-    //     //     .then(response => response.json()) 
-    //     //     .then(json => console.log(json)
-    //     //     )
-    //     //     .catch(err => console.log(err));
-    
-    //     // $.ajax({
-    //     //     url : "/AddOrder",
-    //     //     type: "POST",
-    //     //     data: JSON.stringify(newOrder),
-    //     //     contentType: "application/json; charset=utf-8",
-    //     //      success: function (result) {
-    //     //         console.log(result);
-    //     //     }
-    //     // });
-
-    //SetOrderValues();
-    // }); 
-    
-
+    document.getElementById("buttonDelete").addEventListener("click", function () {
+        deleteOrder(document.getElementById("deleteID").value);      
+    });    
 });  
 // end of wait until document has loaded event  *************************************************************************
+
+function createList() {
+    // update local array from server
+    fetch('/getAllOrders')
+    // Handle success
+    .then(response => response.json())  // get the data out of the response object
+    .then( responseData => fillUL(responseData))    //update our array and li's
+    .catch(err => console.log('Request Failed', err)); // Catch errors
+};
+    
+function fillUL(data) {
+    orderArray = data;
+    console.log(orderArray);
+    // clear prior data
+    var divOrderList = document.getElementById("divOrderList");
+    while (divOrderList.firstChild) {    // remove any old data so don't get duplicates
+        divOrderList.removeChild(divOrderList.firstChild);
+    };
+    
+    var ul = document.createElement('ul');
+       
+    orderArray.forEach(function (element,) {   // use handy array forEach method
+        var li = document.createElement('li');
+        li.innerHTML = element.ID + ":  &nbsp &nbsp  &nbsp &nbsp " + 
+        element.StoreID + "  &nbsp &nbsp  &nbsp &nbsp "  +
+        element.SalesPersonID + " &nbsp &nbsp  &nbsp &nbsp  " + 
+        element.CdID  + " &nbsp &nbsp  &nbsp &nbsp  " + 
+        element.PricePaid  + " &nbsp &nbsp  &nbsp &nbsp  " + 
+        element.Date;
+        ul.appendChild(li);
+    });
+    divOrderList.appendChild(ul);
+}
+    
+function deleteOrder(ID) {
+    fetch('/DeleteOrder/' + ID, {
+        method: "DELETE",
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json), createList())
+    .catch(err => console.log(err));
+}
--- OrderProject/routes/index.js
+++ OrderProject/routes/index.js
@@ -1,17 +1,42 @@
 var express = require('express');
 var router = express.Router();
-var fs = require("fs");
 
-// start by creating data so we don't have to type it in each time
-let ServerOrderArray = [];
+// removed my file management code,
 
-// my file management code, embedded in an object
-fileManager  = {
-  write: function() {
-    let data = JSON.stringify(ServerOrderArray);    // take our object data and make it writeable
-    fs.writeFileSync('ordersData.json', data);  // write it
+// add mongoDB support  ===============================
+
+// mongoose is a API wrapper overtop of mongodb, just like
+// .ADO.Net is a wrapper over raw SQL server interface
+const mongoose = require("mongoose");
+
+const OrderSchema = require("../orderSchema");
+
+
+// edited to include my non-admin, user level account and PW on mongo atlas
+// and also to include the name of the mongo DB that the collection is in (MoviesDB)
+const dbURI = "mongodb+srv://talam:Black1337@tashibcluster.xzyxl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  //add correct connection string above
+
+  // Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
+// by default, you need to set it to false.
+
+
+mongoose.set('useFindAndModify', false);
+
+const options = {
+  reconnectTries: Number.MAX_VALUE,
+  poolSize: 10,
+  useNewUrlParser: true //delete if app has issues
+};
+
+mongoose.connect(dbURI, options).then(
+  () => {
+    console.log("Database connection established!");
   },
-}
+  err => {
+    console.log("Error connecting Database instance due to: ", err);
+  }
+);
 
 
 /* GET home page. */
@@ -19,9 +44,21 @@ router.get('/', function(req, res, next) {
   res.sendFile('index.html');
 });
 
+/* GET all Order data */
+router.get('/getAllOrders', function(req, res) {
+  // find {  takes values, but leaving it blank gets all}
+  OrderSchema.find({}, (err, AllOrders) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+    res.status(200).json(AllOrders);
+  });
+});
+
 //Verify server is getting order
 router.post('/oneOrder', function(req, res) {
-  const newOrder = req.body;  // get the object from the req object sent from browser
+  const newOrder = new OrderSchema(req.body);  // get the object from the req object sent from browser
   console.log(newOrder);
   // prepare a reply to the browser
   var response = {
@@ -32,17 +69,49 @@ router.post('/oneOrder', function(req, res) {
 });
 
 /* Add one new Order */
-router.post('/AddOrder', function(req, res) {
-  const newOrder = req.body;  // get the object from the req object sent from browser
-  console.log(newOrder);
-  ServerOrderArray.push(newOrder);  // add it to our "DB"  (array)
-  fileManager.write();
-  // prepare a reply to the browser
-  var response = {
-    status  : 200,
-    success : 'Added Successfully'
-  }
-  res.end(JSON.stringify(response)); // send reply
+router.post('/addOrder', function(req, res) {
+
+  let oneNewOrder = new OrderSchema(req.body);  
+  console.log(req.body);
+  oneNewOrder.save((err, todo) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else {
+      // console.log(todo);
+      // res.status(201).json(todo);
+      var response = {
+        status  : 200,
+        success : 'Added Successfully'
+      }
+      res.end(JSON.stringify(response)); // send reply
+    }
+  });
+});
+
+router.get('/sortPriceSalesPerson', function(req, res) {
+  // find {  takes values, but leaving it blank gets all}
+  OrderSchema.find({SalesPersonID: "7", PricePaid: {$gt:6, $lt:14} }, (err, AllOrders) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+    res.status(200).json(AllOrders);
+  });
+});
+
+// delete order
+router.delete('/DeleteOrder/:ID', function (req, res) {
+  OrderSchema.deleteOne({ ID: req.params.ID }, (err, note) => { 
+    if (err) {
+      res.status(404).send(err);
+    }
+    var response = {
+      status  : 200,
+      success : 'Order ' +  req.params.ID + ' deleted!'
+    }
+    res.end(JSON.stringify(response)); // send reply
+  });
 });
 
 
